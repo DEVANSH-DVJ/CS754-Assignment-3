5a857a0faf2f10726907ec06ce63ed7d53466ffd
devansh-dvj <devansh.dvj@gmail.com>
2021-03-24 09:14:48 +0530

    Final changes

--- MATLAB/@RU/RU.m
+++ MATLAB/@RU/RU.m
@@ -1,10 +1,10 @@
 function  res = RU(N, M, angles)
 
-res.adjoint = 0;
-res.N = N;
+res.adjoint = 0; % 1 if transpose
+res.N = N; % Width of Image
 res.M = M; % Number of tomographic displacements
-res.Q = size(angles, 2);
-res.angles = angles;
+res.angles = angles; % Angle of projections
+res.Q = size(angles, 2); % Number of angle of projections
 
 % Register this variable as a RU class
 res = class(res, 'RU');
